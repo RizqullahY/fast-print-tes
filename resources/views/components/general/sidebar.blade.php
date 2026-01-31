96effc592c1f3d48c3996b1d2c3e51cf3ab11796
Rafly R. Y. <rizqullahyusufrafly@gmail.com>
2026-01-31 15:04:46 +0700

    CHORE: sidebar update

--- resources/views/components/general/sidebar.blade.php
+++ resources/views/components/general/sidebar.blade.php
@@ -17,22 +17,22 @@
 
     <ul class="menu-inner py-1">
 
-        <li class="menu-item active open">
+        <li class="menu-item {{ Request::is('product*') ? 'active open' : '' }}">
             <a href="/product" class="menu-link">
                 <i class="menu-icon tf-icons ti ti-smart-home"></i>
                 <div data-i18n="Product">Product</div>
             </a>
         </li>
-        <li class="menu-item active open">
-            <a href="/categories" class="menu-link">
+        <li class="menu-item {{ Request::is('category*') ? 'active open' : '' }}">
+            <a href="/category" class="menu-link">
                 <i class="menu-icon tf-icons ti ti-smart-home"></i>
                 <div data-i18n="Categories">Categories</div>
             </a>
         </li>
-        <li class="menu-item active open">
+        <li class="menu-item {{ Request::is('status*') ? 'active open' : '' }}">
             <a href="/status" class="menu-link">
                 <i class="menu-icon tf-icons ti ti-smart-home"></i>
-                <div data-i18n="status">Status</div>
+                <div data-i18n="Status">Status</div>
             </a>
         </li>
 
